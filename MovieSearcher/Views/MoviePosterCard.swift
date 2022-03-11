9682cb08a3944b109784114afa547522c3626efd
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-11 13:05:28 -0800

    folder positions change

--- MovieSearcher/Views/MoviePosterCard.swift
+++ MovieSearcher/Views/MoviePosterCard.swift
@@ -18,13 +18,13 @@ struct MoviePosterCard: View {
                 Image(uiImage: self.imageLoader.image!)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-                    .cornerRadius(8)
+                    .cornerRadius(15)
                     .shadow(radius: 4)
                 
             } else {
                 Rectangle()
                     .fill(Color.gray.opacity(0.3))
-                    .cornerRadius(8)
+                    .cornerRadius(15)
                     .shadow(radius: 4)
                 
                 Text(movie.title)
