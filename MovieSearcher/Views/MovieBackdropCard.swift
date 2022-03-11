196add160ccf9bf368229960e825cee524c46a98
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-11 12:43:52 -0800

    image loader added with UIKIT

---
+++ MovieSearcher/Views/MovieBackdropCard.swift
@@ -0,0 +1,20 @@
+//
+//  MovieBackdropCard.swift
+//  MovieSearcher
+//
+//  Created by Mehdi MMS on 11/03/2022.
+//
+
+import SwiftUI
+
+struct MovieBackdropCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieBackdropCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieBackdropCard()
+    }
+}
