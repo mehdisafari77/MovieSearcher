34b2b15654a31322a59e47ee8415b12104ecc225
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-11 12:19:40 -0800

    create movie model

---
+++ MovieSearcher/Models/Movie.swift
@@ -0,0 +1,26 @@
+//
+//  Movie.swift
+//  MovieSearcher
+//
+//  Created by Mehdi MMS on 11/03/2022.
+//
+
+import Foundation
+
+struct MovieResponse: Decodable {
+    let results: [Movie]
+}
+
+struct Movie: Decodable {
+    
+    let id: Int
+    let title: String
+    let backdropPath: String?
+    let posterPath: String?
+    let overview: String
+    let voteAverage: Double?
+    let voteCount: Int
+    let runtime: Int?
+
+    
+}
