979ac3fb84f2f2ccef978dfb97299f7309ff4cfd
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-11 12:12:20 -0800

    Initial Commit

---
+++ MovieSearcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieSearcher
+//
+//  Created by Mehdi MMS on 11/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieSearcher/MovieSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearcherApp.swift
+//  MovieSearcher
+//
+//  Created by Mehdi MMS on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
